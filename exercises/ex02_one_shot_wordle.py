1d62a39d5851752132830f7143a397389176a6c9
Sadie Amato <saamato@email.unc.edu>
2022-01-27 17:32:18 -0500

    done with ex02

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,43 @@
+"""One shot wordle for the win!"""
+
+___author___ = "730389944"
+
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+secret: str = "python"
+
+guess: str = input(f"What is your {len(secret)}-letter guess? ")
+
+while len(guess) != len(secret):
+    guess: str = input(f"What is your {len(secret)}-letter guess? ")
+
+i: int = 0
+emoji_res: str = ""
+
+while i < len(secret):
+    if guess[i] == secret[i]:
+        emoji_res += GREEN_BOX
+    else:
+        char_present: bool = False
+        alt_index: int = 0
+        while (char_present != True and  alt_index < len(secret)):
+            if guess[i] == secret[alt_index]:
+                char_present = True
+            else:
+                alt_index += 1
+        if char_present:
+            emoji_res += YELLOW_BOX
+        else:
+            emoji_res += WHITE_BOX
+    i += 1
+
+print(emoji_res)
+
+if guess == secret: 
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
+
